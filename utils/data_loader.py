f939f567e7e89e42be51902c4cb5f65b67aaeb41
Timur <kaftaranov.work@gmail.com>
2024-10-30 18:15:39 +0300

    wtf

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -14,28 +14,52 @@ data_generator_handler.setFormatter(data_generator_formatter)
 data_generator_logger.addHandler(data_generator_handler)
 
 # Generator
-def data_generator(X_file_path, y_file_path, batch_size, sequence_length, x_columns, y_column):
+def data_generator(X_file_path, y_file_path, x_columns, y_column, batch_size, sequence_length, overlap):
+    reads_per_chunk = sequence_length/overlap
+
     while True:
         X_generator = pd.read_csv(
             X_file_path, 
-            chunksize=sequence_length,
+            chunksize=overlap,
             usecols=x_columns
             )
         y_generator = pd.read_csv(
             y_file_path, 
-            chunksize=sequence_length,
+            chunksize=overlap,
             usecols=y_column
             )
 
         X_batch = list()
         y_batch = list()
-
+        
+        X_chunk = next(X_generator).values
+        y_chunk = next(y_generator).values
         for i in range(batch_size):
-            X_chunk = next(X_generator).values.reshape(1, 500, 10)
-            y_chunk = next(y_generator).values.reshape(1, 500, 1)
+            X_readed = 0
 
-            X_batch.append(X_chunk)
-            y_batch.append(y_chunk)
+            for i in range(reads_per_chunk-1):
+                X_readed = next(X_generator).values     
+                X_chunk = pd.concat(X_chunk, X_readed, axis=0)
+
+                y_chunk = next(y_generator).values.loc[-1]     
+                print(y_chunk)
+                
+            X_chunk = X_chunk.reshape(1, 500, 10)        
+            X_batch.append(X_chunk.copy())
+            X_chunk = X_readed
+
+            y_chunk = y_chunk.reshape(1, 1, 1)
+            y_batch.append(y_chunk.copy())
+            # yield X_chunk
+            # X_chunk = readed
+                
+            
+            
+            # X_chunk = next(X_generator).values.reshape(1, 500, 10)
+            #y_chunk = next(y_generator).values.reshape(1, 500, 1)
+
+            #X_batch.append(X_chunk)
+            #y_batch.append(y_chunk)
 
         X_batch = np.concatenate(X_batch, axis=0)
         y_batch = np.concatenate(y_batch, axis=0)
