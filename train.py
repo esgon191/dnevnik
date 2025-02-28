e3433b3cc46996964c0768864471e3d53b1d46e7
esgon191 <kaftaranov.work@gmail.com>
2025-02-28 12:36:40 +0300

    Убрал .keras из формата сохранения модели и вынес его в отдельную директорию

--- train.py
+++ train.py
@@ -72,7 +72,7 @@ model.fit(train_dataset, epochs=config.EPOCHS)
 
 train_logger.info('Learning ended')
 name = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-model.save(f'./models/{name}.keras') 
+model.save(f'./models/{name}') 
 
 # Тестирование модели 
 train_logger.info('Testing started')
